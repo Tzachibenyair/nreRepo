2b2ca87851a5add7c7e76e2f8b5feb341c9db58d
Tzachibenyair <tzachi666@gmail.com>
2021-10-26 20:05:28 +0300

    after rebabse ,change the data type

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -2,22 +2,22 @@ import java.util.ArrayList;
 
 public class SharedData 
 {
-<<<<<<< HEAD
+
 	private ArrayList<Integer> array;
-=======
-	private ArrayList<Integer> array ;
->>>>>>> branch 'master' of https://github.com/Tzachibenyair/nreRepo
+
+
+
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
 	
-<<<<<<< HEAD
-	public SharedData(ArrayList<Integer> array2, int b) {
-=======
+
+
+
 	public SharedData(ArrayList<Integer> array , int b) {
->>>>>>> branch 'master' of https://github.com/Tzachibenyair/nreRepo
+
 		
-		this.array = array2;
+		this.array = array;
 		this.b = b;
 	}
 
